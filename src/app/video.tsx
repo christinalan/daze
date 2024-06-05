c2f4a4a37e1ba10d358a993daab6b2a58e70341c
Kostadin Krajcev <kokikr@gmail.com>
2024-06-05 03:28:58 -0400

    optimize video and strip audio

--- src/app/video.tsx
+++ src/app/video.tsx
@@ -61,15 +61,15 @@ export const Video: React.FC<VideoProps> = ({containerId, homeClicked}) => {
             ref={videoRef} 
             autoPlay
             playsInline
-            controls={false}
-            muted={true}
+            muted
             loop
+            controls={false}
             placeholder="blur"
             poster="poster-min.png"
             className="fixed object-cover w-full h-full">
-            <source src="video.mp4" type="video/mp4"/>
+                <source src="video.mp4" type="video/mp4"/>
 
-            Your browser does not support the video tag.
+                Your browser does not support the video tag.
             </video>
 
         </>
